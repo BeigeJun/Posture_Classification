f5aa42344b949d6a24d1683675b98afe081e63c2
Seo_Jun_ <wns201401@naver.com>
2024-08-24 00:11:55 +0900

    .

--- 5th_Try/KepointModelOnnx.py
+++ 5th_Try/KepointModelOnnx.py
@@ -1,16 +1,10 @@
 import torch
 from torchvision.models.detection import keypointrcnn_resnet50_fpn, KeypointRCNN_ResNet50_FPN_Weights
 
-# 디바이스를 CPU로 설정
-device = torch.device('cpu')
+model = keypointrcnn_resnet50_fpn(weights=KeypointRCNN_ResNet50_FPN_Weights.COCO_V1)
 
-# 사전 학습된 Keypoint R-CNN 모델 로드 (weights 인수 사용)
-model = keypointrcnn_resnet50_fpn(weights=KeypointRCNN_ResNet50_FPN_Weights.COCO_V1).to(device).eval()
+dummy_input = torch.randn(1, 3, 224, 224)
 
-# 샘플 입력 생성 (배치 크기 1, 3 채널, 224x224 이미지) - 모든 텐서는 동일한 장치에 있어야 함
-dummy_input = torch.randn(1, 3, 224, 224).to(device)
-
-# ONNX 파일로 내보내기
 torch.onnx.export(
     model,                # 변환할 모델
     dummy_input,          # 모델의 입력 샘플
@@ -23,5 +17,3 @@ torch.onnx.export(
     dynamic_axes={'input': {0: 'batch_size'},    # 동적 배치 크기
                   'output': {0: 'batch_size'}}
 )
-
-print("ONNX 모델이 성공적으로 저장되었습니다.")
